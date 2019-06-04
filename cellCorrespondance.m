3125443b408e45150a0777ecae23f38bb5c98781
mattminder <myfiles@tsf-452-wpa-7-170.epfl.ch>
2019-06-04 13:56:24 +0200

    New cell correspondance

--- cellCorrespondance.m
+++ cellCorrespondance.m
@@ -23,37 +23,46 @@ for cell = allCurrCells
     % Get number of counts 
     counts = histcounts(catPrev(curr==cell), catAllPrevCells); 
     [maxCounts, maxCountIx] = max(counts);
-    corresp(cell) = allPrevCells(maxCountIx);
+    
+%     % Check if new cell (i.e. matches to background), set it to max+1
+      corr = allPrevCells(maxCountIx);
+%     if corr <= 0
+%         corresp(cell) = max([allPrevCells, corresp']) + 1;
+%     else
+%         corresp(cell) = corr;
+%     end
+    % commenting the above snippet creates a memory of background
+    corresp(cell) = corr;
     allMaxCounts(cell) = maxCounts;
 end
 
-% Ensure one-to-one mapping
-for cell = allPrevCells
-     % Disregard bg and border
-    if cell <= 0
-        continue
-    end
-
-    ix = find(corresp == cell);
-    
-    % Handle case that more than one map to the same previous cell
-    if length(ix) > 1
-        [~, maxix] = max(allMaxCounts(ix));
-        nomaxix = ix(ix ~= maxix);
-        
-        % Change those index that don't cover the maximal area of the
-        % previous cell to a new index
-        for i = nomaxix
-            corresp(i) = max(corresp) + 1;
-        end
-    end
-    
-    % Handle case that no cell maps to previous cell
-    if isempty(ix)
-        % TODO: What?
-    end
-end
-    
+% % Ensure one-to-one mapping
+% for cell = allPrevCells
+%      % Disregard bg and border
+%     if cell <= 0
+%         continue
+%     end
+% 
+%     ix = find(corresp == cell);
+%     
+%     % Handle case that more than one map to the same previous cell
+%     if length(ix) > 1
+%         [~, maxix] = max(allMaxCounts(ix));
+%         nomaxix = ix(ix ~= maxix);
+%         
+%         % Change those index that don't cover the maximal area of the
+%         % previous cell to a new index
+%         for i = nomaxix
+%             corresp(i) = max(corresp) + 1;
+%         end
+%     end
+%     
+%     % Handle case that no cell maps to previous cell
+%     if isempty(ix)
+%         % TODO: What?
+%     end
+% end
+%     
 % Change the current values of cells
 mask = curr;
 for cell = allCurrCells
